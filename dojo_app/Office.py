93183679635febac102f9cfcb529f5452009d0fa
mariammwanje <mariamnanono498gmail.com>
2017-04-26 11:55:10 +0300

    Add ition of UML Diagram

--- dojo_app/Office.py
+++
@@ -1,8 +0,0 @@
-#importing from class Room
-from .import Room
-
-#creating class office
-class Office(Room):
-    def __init__(self):
-        self.max_number = 6
-        self.occupants = []
--- dojo_app/Staff.py
+++
@@ -1,8 +0,0 @@
-#importing from class Person
-from .import Person
-
-#creating class person
-class Staff(Person):
-    def __init__(self, role, ):
-        self.role = role
-        self.office = None
