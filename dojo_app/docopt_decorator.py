0738cc0b582911fd9d2083f58ee39f411463f8e7
mariammwanje <mariamnanono498gmail.com>
2017-04-26 21:04:30 +0300

    Addition of the interactive allocator Class  and docopt_decorator more functionality

--- dojo_app/docopt_decorator.py
+++ dojo_app/docopt_decorator.py
@@ -1,40 +1,32 @@
-"""
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
-
-Usage:
-    Command
-"""
-
-import cmd
-import sys
-
-from docopt import docopt
-
-from docopt_decorator import docopt_cmd
-from dojo_app.Dojo import Dojo
-
-andela_dojo = Dojo()
-
-class RoomInteractive(cmd.Cmd):
-    intro = 'Welcome to  Andela Dojo Space Allocator Program'
-    prompt = 'Command  '
-
-    @docopt_cmd
-    def do_create_room(self, arg):
-        """Usage: create_room <room_name> <room_type>"""
-        room_name = arg['<room_name>']
-        room_type = arg['<room_type>']
-
-        andela_dojo.create_room(room_name, room_type)
-
-        print(room_type + 'called' + room_name + 'has been creatd')
-
-    @docopt_cmd
-    def do_add_person(self, arg):
-        """"Usage:  add_person <person_name> <person_type>"""
-        print(arg)
+from docopt import docopt, DocoptExit
 
 
-opt = docopt(__doc__, sys.argv[1:])
-RoomInteractive().cmdloop()
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('Invalid Command!')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
