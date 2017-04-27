0e0a9ed2471088ae7f3630247dadbdada99290c5
mariammwanje <mariamnanono498gmail.com>
2017-04-27 19:15:16 +0300

    addition of add room function

--- dojo_app/Fellow.py
+++ dojo_app/Fellow.py
@@ -7,6 +7,6 @@ from dojo_app.Person import Person
 
 class Fellow(Person):
     def __init__(self, person_name, wants_accomodation ):
-        pass
+        # pass
         #inheriting to from class Person the person_name and type fellow
         super(Fellow, self).__init__( "fellow",person_name, wants_accomodation )
--- dojo_app/Office.py
+++ dojo_app/Office.py
@@ -9,3 +9,4 @@ class Office(Room):
         #inheritating from class Room room name and room type
         Room.__init__(Room, room_name, 'office')
         self.max_no_persons = 6
+
